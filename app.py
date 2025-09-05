3b2e09952116e0065a49a963b0d47afabae96e2d
kaanami <a2d2s2zekk@gmail.com>
2025-09-05 15:14:57 +0545

    Improved Model

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ st.title('Diabetes Prediction App')
 
 st.header('Enter your data: ')
 glucose = st.number_input('Glucose', 0,400)
-# dfp = st.number_input('DFP', 0,15)
+dfp = st.number_input('DFP', 0,15)
 age = st.number_input('Age', 0,100)
 
 if st.button('Predict'):
-    to_predict = np.array([[glucose, age]])  # dfp
+    to_predict = np.array([[glucose, age]])
 
     result = predict_diabetes(to_predict)
     print(result)
--- diabetes.py
+++ diabetes.py
@@ -1,7 +1,7 @@
 import joblib
 
 def predict_diabetes(input):
-    model = joblib.load('saved/DTree_Diabetes.pkl')
+    model = joblib.load('saved/dGmodel.pkl')
 
     result = model.predict(input)
     return result
